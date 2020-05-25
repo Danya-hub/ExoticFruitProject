b9cf600a1fc42338433a6f22e3a0bd1f43399f63
Danya <6694524@gmail.com>
2020-05-25 13:04:41 +0300

    my first commit

--- components/header/header.js
+++ components/header/header.js
@@ -1,17 +1,50 @@
 'use strict';
 
-// import {fruit, fruitStockIncludes, fruitSet, fruitSetStockIncludes} from "";
+import { fruit, fruitSet } from "../../data.js";
+
+// import {fruit, fruitStockIncludes, fruitSet, fruitSetStockIncludes} from './data.js';
 
 const header = () => {
     const headerList = document.querySelector('.header__list'); //ul
     const headerListLinkActive = document.querySelector('.header__list-li-linkActive'); //a
-    const pushDataMain = document.querySelector('.groceryCards__text'); //p
+    const groceryCardsList = document.querySelector('.groceryCards__list'); //ul
+    const getMarkup = (array) => {
+        let markup = '';
+        for (const item of array) {
+            markup += `
+            <li class="groceryCards__list-item">
+                <h2 class="groceryCards__list-item-title">${item.title}</h2>
+                <img src="${item.photoProduct}" class="groceryCards__list-item-img">
+            </li>
+            `
+        }
+        return markup
+    }
+    groceryCardsList.innerHTML = getMarkup(fruit);
 
-    pushDataMain.textContent = headerListLinkActive.dataset.link;
-
-    headerList.addEventListener('click', (event) => {
-        pushDataMain.textContent = event.target.getAttribute('data-link');
-    });
+    const linkActiveMain = (e) => {
+        // console.log(e.target);
+        if (e.target === e.currentTarget) {
+            return
+        } else {
+            switch (e.target.dataset.link) {
+                case 'fruit':
+                    groceryCardsList.innerHTML = getMarkup(fruit);
+                    break;
+                case 'fruitSet':
+                    groceryCardsList.innerHTML = getMarkup(fruitSet);
+                    break;
+                default:
+                    break;
+            }
+            
+            // const headerListLinkActive = document.querySelector('.header__list-li-linkActive');
+            // headerListLinkActive && headerListLinkActive.classList.remove('headerListLinkActive');
+            // e.target.classList.add('headerListLinkActive');
+            console.log(e.target.dataset.link);
+        }
+    }
+    headerList.addEventListener('click', linkActiveMain);
 }
 
 export default header;
---
+++ components/pages/fruits.js
@@ -0,0 +1,8 @@
+let 
+
+const createListCard = (e) => {
+    for (let i = 0; i < e.length; i++) {
+        e.innerHtml = '
+        '
+    }
+}
